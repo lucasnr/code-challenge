3ba41f13e7484e9f3fc430d69e94b487e6664451
Lucas Nascimento <lucasnascimentoribeiro13@gmail.com>
2019-10-11 18:13:20 -0300

    Iniciando a estilização o conteúdo principal

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from 'react';
 
 import Header from './component/Header';
 import { Profile, Contact, Skills } from './component/Sidebar';
+import { Experience } from './component/Main';
 
 import './assets/css/reset.css';
+import './assets/css/style.css';
 import './assets/css/sidebar.css';
 
 class App extends React.Component {
@@ -11,7 +13,7 @@ class App extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			user: {
+			profile: {
 			}
 		}
 	}
@@ -20,21 +22,26 @@ class App extends React.Component {
 		fetch("http://www.mocky.io/v2/5a5e38f3330000b0261923a5")
 			.then(response => response.json())
 			.then(user => {
-				this.setState({ user });
+				const { profile } = user;
+				this.setState({ profile });
 			})
 	}
 
 	render() {
 		return (
-			<>
+			<main className="main">
 				<aside className="sidebar">
-					<Header {...this.state.user.profile} />
-					<Profile {...this.state.user.profile} />
-					<Contact {...this.state.user.profile} />
-					<Skills {...this.state.user.profile} />
+					<Header {...this.state.profile} />
+					<Profile {...this.state.profile} />
+					<Contact {...this.state.profile} />
+					<Skills {...this.state.profile} />
 				</aside>
+				<article className="mainContent">
+					<h2 className="mainContent-title">Work Experience</h2>
 
-			</>
+					<Experience />
+				</article>
+			</main>
 		);
 	}
 }
--- src/component/Header/index.js
+++ src/component/Header/index.js
@@ -13,7 +13,7 @@ export default function Header({image, name, profession}) {
 							alt="User avatar" className="photoCard-image" />
 					</div>
 				</div>
-				<h2 className="name">{name}</h2>
+				<h1 className="name">{name}</h1>
 				<span className="profession">{profession}</span>
 				<hr className="separator" />
 			</div>
